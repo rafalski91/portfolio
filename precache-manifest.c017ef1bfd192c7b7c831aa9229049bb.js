d1ed0212b65aff50bbcff91d7a926dbb22ed75ea
Rafal <rafalweglinski@protonmail.com>
2019-08-26 15:35:12 +0200

    Updates

---
+++ precache-manifest.c017ef1bfd192c7b7c831aa9229049bb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6beb614403de584c12941d14cd1537a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "15fada88f189df20cc2f",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7d999a225a7b32817cda",
+    "url": "/portfolio/static/js/2.e7fdb683.chunk.js"
+  },
+  {
+    "revision": "15fada88f189df20cc2f",
+    "url": "/portfolio/static/js/main.8d275d87.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "02bd8151b9ce19361a178e99b4a64de3",
+    "url": "/portfolio/static/media/ProjectHtml.02bd8151.jpg"
+  },
+  {
+    "revision": "b3cc4ee6e7c83e954cec9d0f5a31793d",
+    "url": "/portfolio/static/media/ProjectJavaScript.b3cc4ee6.jpg"
+  },
+  {
+    "revision": "a911af444741b54f5d60a0c43a30befa",
+    "url": "/portfolio/static/media/ProjectReact.a911af44.jpg"
+  },
+  {
+    "revision": "e7324300fd67d3239e682077c05bac9c",
+    "url": "/portfolio/static/media/aerial.e7324300.jpg"
+  },
+  {
+    "revision": "d6095a8d4754b523376374d07120a155",
+    "url": "/portfolio/static/media/bootstrap2.d6095a8d.jpg"
+  },
+  {
+    "revision": "15f7470d3c39ace1d3fa2a3d5485cac7",
+    "url": "/portfolio/static/media/css32.15f7470d.jpg"
+  },
+  {
+    "revision": "7a401ff260b8056638453907c473566a",
+    "url": "/portfolio/static/media/gimp2.7a401ff2.jpg"
+  },
+  {
+    "revision": "0b93f04bd82d23e9b2c1c70fd9288347",
+    "url": "/portfolio/static/media/github2.0b93f04b.jpg"
+  },
+  {
+    "revision": "029b5e5075f87f009347682d09120663",
+    "url": "/portfolio/static/media/html52.029b5e50.jpg"
+  },
+  {
+    "revision": "e4b22af8f9508702a6dda02aace99fe9",
+    "url": "/portfolio/static/media/javascript2.e4b22af8.jpg"
+  },
+  {
+    "revision": "ca2a6d1f06b62cb76b141c07789e0f22",
+    "url": "/portfolio/static/media/mysql2.ca2a6d1f.jpg"
+  },
+  {
+    "revision": "7f052b980c0a4533706deb1d0ff2275d",
+    "url": "/portfolio/static/media/react2.7f052b98.jpg"
+  },
+  {
+    "revision": "89a28767d43ab414ecb59bdb09e70970",
+    "url": "/portfolio/static/media/wordpress2.89a28767.jpg"
+  }
+]);
